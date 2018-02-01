e64dd84831e9fd176deb5c5ec4572feb95e37d0b
Amilkhael <amilkhael@outlook.com>
2018-02-01 07:31:08 -0600

    Estilos de menú

--- index.php
+++ index.php
@@ -88,8 +88,8 @@
 
 </div>
 <header class="container">
-    <nav class="navbar navbar-expand-lg fixed-top navbar-light bg-light">
-     <a class="navbar-brand" href="#">Logo</a>
+    <nav class="navbar navbar-expand-lg fixed-top navbar-light ">
+     <a class="navbar-brand" href="#"></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
