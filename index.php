f8d1876934fc60dbd0a5ac2b4ab2e3bacb14e490
Amilkhael <amilkhael@outlook.com>
2018-01-31 19:02:05 -0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html lang="es-419">
+
+<head>
+
+  <!--Metas -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Logros y avances de los últimos 3 años en nuestra Universidad: Docencia, Extensión, Investigación, Identidad y Gestión institucional.">
+    <meta name="author" content="La Salle México">
+    <!--Facebook -->
+    <meta property="og:url" content="http://informerector.lasalle.mx"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:image" content="http://informerector.lasalle.mx/images/FACEBOOK_PREVIEW.png" />
+    <meta property="og:title" content="Comunicado de Gestión 2014-2017" />
+    <meta property="og:description" content="Logros y avances de los últimos 3 años en nuestra Universidad: Docencia, Extensión, Investigación, Identidad y Gestión institucional.">
+    <!-- Twitter-->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@RectorLaSalle">
+    <meta name="twitter:creator" content="@LaSalle_MX">
+    <meta name="twitter:title" content="Comunicado de Gestión 2014-2017">
+    <meta name="twitter:description" content="Logros y avances de los últimos 3 años en nuestra Universidad: Docencia, Extensión, Investigación, Identidad y Gestión institucional.">
+    <meta name="twitter:image" content="http://informerector.lasalle.mx/images/TWITTER_CARD.png">
+
+    <title>Encuetro IALU 2018 | La Salle</title>
+
+    <!-- Bootstrap Core CSS -->
+    <!--<link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- Custom Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700,900" rel="stylesheet">
+
+    <!-- Plugin CSS -->
+
+
+    <!-- Theme CSS -->
+    <link href="css/custom.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <link rel="shortcut icon" href="images/favicon/favicon.ico" type="image/x-icon">
+</head>
+
+<body id="page-top">
+<!-- Twitter -->
+<script>window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+
+  return t;
+}(document, "script", "twitter-wjs"));</script>
+<!-- Fin Twitter -->
+
+<div >
+
+</div>
+<header class="container">
+    <nav class="navbar navbar-expand-lg fixed-top navbar-light bg-light">
+     <a class="navbar-brand" href="#">Logo</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-item nav-link active" href="#">Inicio <span class="sr-only">(current)</span></a>
+        <a class="nav-item nav-link" href="#">Conferencias</a>
+        <a class="nav-item nav-link" href="#">Mesas de reflexión</a>
+        <a class="nav-item nav-link" href="#">Talleres</a>
+        <a class="nav-item nav-link" href="#">Ensayo integrador</a>
+        <a class="nav-item nav-link" href="#">Feria Cultural</a>
+      </div>
+    </div>
+  </nav>
+</header>
+
+<!-- Slider -->
+<div class="container-fluid">
+  <div class="row justify-content-center" >
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+
+            <img class="d-block" src="http://4.bp.blogspot.com/-ay6bnwkAcc4/ULoQkXsUSdI/AAAAAAAAEkY/0mPmofhgQxo/s1600/paisaje-banner.jpg" alt="First slide">
+
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Título</h5>
+            <p>Contenido</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+
+            <img class="d-block w-100" src="https://i.pinimg.com/originals/59/ce/e3/59cee34e298f66560837e0b29a7e2f2c.jpg" alt="Second slide">
+
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Título</h5>
+            <p>Contenido</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+
+            <img class="d-block w-100" src="https://www.galeonplaya.com/sites/default/files/styles/slides/public/paisaje_2.jpg?itok=W1dGtSt0" alt="Third slide">
+
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleSlidesOnly" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Anterior</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleSlidesOnly" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Siguiente</span>
+      </a>
+    </div>
+  </div>
+</div>
+<!-- Fin Slider -->
+<div class="container">
+  <h2>No olvides que...</h2>
+  <p>Recepción de experiencias 1 marzo 2018 al 30 mayo 2018</p>
+  <p>Inscripciones: 1 de septiembre de 2018</p>
+  <p>Cierre de Inscripciones: 15 de febrero de 2019</p>
+</div>
+<section class="container">
+    <h2>Programa</h2>
+    <div class="row">
+      <div class="col-sm-4">
+        <p>Día 1</p>
+        <div class="row">
+          <div class="col-sm-6">08:00 – 09:00</div>
+          <div class="col-sm-6">Recepción</div>
+          <div class="col-sm-6">09:00 – 09:30</div>
+          <div class="col-sm-6">Acto Inaugural</div>
+          <div class="col-sm-6">09:30 – 10:30</div>
+          <div class="col-sm-6">Conferencia Magistral: Llamadas y respuestas educativas</div>
+          <div class="col-sm-6">10:30 – 11:00</div>
+          <div class="col-sm-6">Descanso</div>
+          <div class="col-sm-6">11:00 – 12:00</div>
+          <div class="col-sm-6">Mesa de diálogo con expertos</div>
+          <div class="col-sm-6">12:00 – 13:15</div>
+          <div class="col-sm-6">Mesas de Reflexión</div>
+          <div class="col-sm-6">13:15 – 14:30</div>
+          <div class="col-sm-6">Comida</div>
+          <div class="col-sm-6">14:30 – 15:30</div>
+          <div class="col-sm-6">Talleres Simultáneos 1 / Presentaciones 1</div>
+          <div class="col-sm-6">15:30 – 15:45</div>
+          <div class="col-sm-6">Descanso</div>
+          <div class="col-sm-6">15:45 – 16:45</div>
+          <div class="col-sm-6">Talleres Simultáneos 2 / Presentaciones 2</div>
+          <div class="col-sm-6">16:45 – 17:30</div>
+          <div class="col-sm-6">Mesa Magistral de Reflexión. Sesión de Preguntas</div>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <p>Día 2</p>
+      </div>
+      <div class="col-sm-4">
+        <p>Día 3</p>
+      </div>
+    </div>
+</section>
+
+<?php include 'footer.php';?>
