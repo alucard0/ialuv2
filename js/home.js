0123b0b74cb91172d8a42ef172807e48548ff251
Amilkhael <amilkhael@outlook.com>
2018-03-06 11:18:15 -0600

    Carga y Configuración Tipografía

--- js/home.js
+++ js/home.js
@@ -39,148 +39,39 @@ jQuery(function($){
 	});
 });
 
-//Funcion para scroll
-(function($) {
-    "use strict"; // Start of use strict
-
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $(document).on('click', 'a.page-scroll', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  //.not('[href="#"]')
+  //.not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
         event.preventDefault();
-    });
-
-    // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 100
-    });
-
-    // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function() {
-        $('.navbar-toggle:visible').click();
-    });
-
-    // Offset for Main Navigation
-    $('#mainNav').affix({
-        offset: {
-            top: 50
-        }
-    })
-
-})(jQuery); // End of use strict
-
-;(function($) {
-
-$.fn.letterDrop = function() {
-  // Chainability
-  return this.each( function() { 
-  
-  var obj = $( this );
-  
-  var drop = {
-    arr : obj.text().split( '' ),
-    
-    range : {
-      min : 1,
-      max : 9
-    },
-    
-    styles : function() {
-      var dropDelays = '\n', addCSS;
-      
-       for ( i = this.range.min; i <= this.range.max; i++ ) {
-         dropDelays += '.ld' + i + ' { animation-delay: 1.' + i + 's; }\n';  
-       }
-      
-        addCSS = $( '<style>' + dropDelays + '</style>' );
-        $( 'head' ).append( addCSS );
-    },
-    
-    main : function() {
-      var dp = 0;
-      obj.text( '' );
-      
-      $.each( this.arr, function( index, value ) {
-
-        dp = dp.randomInt( drop.range.min, drop.range.max );
-        
-        if ( value === ' ' )
-          value = '&nbsp';
-        
-          obj.append( '<span class="letterDrop ld' + dp + '">' + value + '</span>' );
-        
-      });
-          
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
     }
-  };
-   
-  Number.prototype.randomInt = function ( min, max ) {
-    return Math.floor( Math.random() * ( max - min + 1 ) + min );
-  };
-  
-  
-  // Create styles
-  drop.styles();
-
-
-    // Initialise
-    drop.main();
   });
-
-};
-
-}(jQuery));
-
-
-//Cambio de Imago
-jQuery(function($){
-
-$(window).scroll(function () {
-    if ($(document).scrollTop() == 0 && $(this).width() > 500) {
-        $('#imago01').removeAttr('src');
-        $('#imago01').attr("src","images/imagotipo/imago_blanco-01.png");
-
-        $('#imago02').removeAttr('src');
-        $('#imago02').attr("src","images/imagotipo/imago_blanco-02.png");
-
-        $('#imago03').removeAttr('src');
-        $('#imago03').attr("src","images/imagotipo/imago_blanco-03.png");
-    }
-    else
-    {
-      $('#imago01').removeAttr('src');
-      $('#imago01').attr("src","images/imagotipo/imago_color-01.png");
-
-      $('#imago02').removeAttr('src');
-      $('#imago02').attr("src","images/imagotipo/imago_color-02.png");
-
-      $('#imago03').removeAttr('src');
-      $('#imago03').attr("src","images/imagotipo/imago_color-03.png");
-    }
-}); 
-
-
-if ($( "#mainNav" ).hasClass( "affix-top" ) && $(this).width() < 500) {
-      $('#imago01').removeAttr('src');
-      $('#imago01').attr("src","images/imagotipo/imago_color-01.png");
-
-      $('#imago02').removeAttr('src');
-      $('#imago02').attr("src","images/imagotipo/imago_color-02.png");
-
-      $('#imago03').removeAttr('src');
-      $('#imago03').attr("src","images/imagotipo/imago_color-03.png");
-}
-
-});
-
-jQuery(function($){
-  $('.panel-collapse').on('shown.bs.collapse', function(e) {
-    var $panel = $(this).closest('.panel');
-    $('html,body').animate({
-      scrollTop: $panel.offset().top
-    }, 500);
-  });
-});
