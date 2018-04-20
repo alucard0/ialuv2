2423fe0a82e4e5d2d14debe8fc4a0651c44e37a3
Angellomon <angelmtzdiaz@gmail.com>
2018-04-20 14:10:51 -0500

    Cambios al footer
    :)

--- footer.php
+++ footer.php
@@ -6,55 +6,121 @@
 ?>
 	<footer class="container-fluid">
 		<div class="row bgGray">
-			<div class="col-sm-2 col-xs-6">
-				<figure><a href="http://redlasalle.mx/" target="_blank"><img src="images/footer/RED_LA_SALLE.png" alt="La Salle red de universidades" class="img-responsive"></a></figure>
+			<!-- Red La Salle y direccion -->
+			<div class="col-sm-12 col-md-4 footer-col">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-12">
+							<figure class="logos"><a href="http://www.lasalle.mx/" target="_blank"><img src="images/footer/Footer_Logo_Imagotipo.png" alt="Universidad La Salle" class="img-responsive" align="left"></a></figure>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-12">
+							<p class="direccionFooter">Benjamín Franklin 45, Col. Condesa, C.P. 06140, <br>México, Ciudad de México
+							<br>Tel. +52 1 5278-9500</p>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-12">
+							<figure class="logos"><a href="http://www.lasalle.mx/" target="_blank"><img src="images/footer/Footer_Logo_Universidad-Segura.png" alt="Universidad Segura" class="img-responsive" align="left"></a></figure>
+						</div>
+					</div>
+				</div>
 			</div>
-			<div class="col-sm-2 col-xs-6">
-				<figure><img src="images/footer/RED_LA_SALLE_2.png" alt="Universidad La Salle" class="img-responsive"></figure>
+			<!-- Siguenos -->
+			<div class="col-sm-12 col-md-4 footer-col bloque-social">
+				<!-- <div class="container-fluid"> -->
+					<div class="row redesSociales">
+						<div class="col-5 col-md-12 footer-col">
+							<figure class="siguenos">
+								<img id="siguenos_" src="images/footer/333333-0.png" alt="Síguenos" class="img-fluid">
+							</figure>
+						</div>
+						<div class="col-7 col-md-12 redesSociales">
+							<div class="container-fluid">
+								<div class="row redesSociales">
+									<div class="col-3">
+										<figure>
+											<a href="https://www.facebook.com/LaSalleMX" target="_blank"><img id="_facebook" src="images/footer/333333-0.png" alt="Síguenos en Facebook" class="img-responsive"></a>
+										</figure>
+									</div>
+									<div class="col-3">
+										<figure>
+											<a href="https://twitter.com/lasalle_mx" target="_blank"><img id="_twitter" src="images/footer/333333-0.png" alt="Síguenos en Twitter" class="img-responsive"></a>
+										</figure>
+									</div>
+									<div class="col-3">
+										<figure>
+											<a href="https://www.instagram.com/lasalle_mx/" target="_blank"><img id="_instagram" src="images/footer/333333-0.png" alt="Síguenos en Instagram" class="img-responsive"></a>
+										</figure>
+									</div>
+									<div class="col-3">
+										<figure>
+											<a href="https://www.linkedin.com/school/24227?pathWildcard=24227" target="_blank"><img id="_linkedin" src="images/footer/333333-0.png" alt="Síguenos en Linkedin" class="img-responsive"></a>
+										</figure>
+									</div>
+								</div>
+								<div class="row redesSociales">
+									<div class="col-3">
+										<figure>
+											<a href="https://es.foursquare.com/v/universidad-la-salle/4b8ffab0f964a520c16d33e3?tipsPage=2" target="_blank"><img src="images/footer/333333-0.png" id="_swarm" alt="Comparte tu experiencia en Foursquare" class="img-responsive"></a>
+										</figure>
+									</div>
+									<div class=" col-3">
+										<figure>
+											<a href="https://es.foursquare.com/v/universidad-la-salle/4b8ffab0f964a520c16d33e3?tipsPage=2" target="_blank"><img src="images/footer/333333-0.png" id="_foursquare" alt="Comparte tu experiencia en Foursquare" class="img-responsive"></a>
+										</figure>
+									</div>
+									<div class="col-3">
+										<figure>
+											<a href="https://www.snapchat.com/add/lasalleMX" target="_blank"><img src="images/footer/333333-0.png" id="_snapchat" alt="" class="img-responsive"></a>
+										</figure>
+									</div>
+									<div class="col-3">
+										<figure>
+											<a href="https://www.youtube.com/user/LaSalleMX" target="_blank"><img src="images/footer/333333-0.png" id="_youtube" alt="Síguenos en Youtube" class="img-responsive"></a>
+										</figure>
+									</div>
+								</div>
+							</div>
+						</div>
+					<!-- </div> -->
+				</div>
 			</div>
-			<div class="col-sm-8 redesSociales">
-				<figure>
-					<a href="https://www.facebook.com/LaSalleMX" target="_blank"><img src="images/footer/FACEBOOK.png" alt="Síguenos en Facebook" class="img-responsive"></a>
-				</figure>
-				<figure>
-					<a href="https://twitter.com/lasalle_mx" target="_blank"><img src="images/footer/TWITTER.png" alt="Síguenos en Twitter" class="img-responsive"></a>
-				</figure>
-				<figure>
-					<a href="https://www.instagram.com/lasalle_mx/" target="_blank"><img src="images/footer/INSTAGRAM.png" alt="Síguenos en Instagram" class="img-responsive"></a>
-				</figure>
-				<figure>
-					<a href="https://www.linkedin.com/school/24227?pathWildcard=24227" target="_blank"><img src="images/footer/LINKEDIN.png" alt="Síguenos en Linkedin" class="img-responsive"></a>
-				</figure>
-				<figure>
-					<a href="https://es.foursquare.com/v/universidad-la-salle/4b8ffab0f964a520c16d33e3?tipsPage=2" target="_blank"><img src="images/footer/SWARM.png" alt="Comparte tu experiencia en Foursquare" class="img-responsive"></a>
-				</figure>
-				<figure>
-					<a href="https://es.foursquare.com/v/universidad-la-salle/4b8ffab0f964a520c16d33e3?tipsPage=2" target="_blank"><img src="images/footer/FOURSQUA.png" alt="Comparte tu experiencia en Foursquare" class="img-responsive"></a>
-				</figure>
-				<figure>
-					<a href="https://www.snapchat.com/add/lasalleMX" target="_blank"><img src="images/footer/SNAPCHAT.png" alt="" class="img-responsive"></a>
-				</figure>
-				<figure>
-					<a href="https://www.youtube.com/user/LaSalleMX" target="_blank"><img src="images/footer/YOUTUBE.png" alt="Síguenos en Youtube" class="img-responsive"></a>
-				</figure>
-				<p>Benjamín Franklin 45 Col. Condesa. Del. Cuauhtémoc Ciudad de México, 06140. <span>(+52) 5278-9500</span></p>
+			<!-- Otros links -->
+			<div class="col-sm-12 col-md-4 footer-col">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-4 col-sm-12">
+							<figure class="logos"><img src="images/footer/Footer_Logo_Profesionales.png" alt="Profesionales Con Valor" class="img-fluid" align="right"></figure>
+						</div>
+					
+					<!-- <div class="row"> -->
+						<div class="col-4 col-sm-12">
+							<figure class="logos"><a href="http://ialu.org/english/" target="_blank"><img src="images/footer/Footer_Logo_IALU.png" alt="La Salle Universities" class="img-fluid" align="right"></a></figure>
+						</div>
+					<!-- </div> -->
+					<!-- <div class="row"> -->
+						<div class="col-4 col-sm-12">
+							<figure class="logos"><a href="http://redlasalle.mx/" target="_blank"><img src="images/footer/Footer_Logo_RedLaSalle.png" alt="La Salle red de universidades" class="img-fluid" align="right"></a></figure>
+						</div>
+					<!-- </div> -->
+					</div>
+				</div>
 			</div>
 		</div>
-		<div class="container-fluid">
-			<div class="row bgLigthGray justify-content-center">
-				<p>Profesionales con <span class="textoValor">Valor</span></p>
-			</div>
+		<div class="row bgBlack" >
+			<p>© Universidad La Salle A.C. 2017 Todos los derechos Reservados.</p>
 		</div>
-		<div class="container-fluid bgWhite">
-			<div class="row">
-				<div class="col col-sm-6 col-xs-6">
+		<div class="row bgWhite">
+				<div class="col-sm-6 col-6">
 					<p class="aviso">Aviso de privacidad</p>
 				</div>
-				<div class="col col-sm-6 col-xs-6">
+				<div class="col-sm-6 col-6">
 					<p class="terminos">Términos y condiciones</p>
 				</div>
 
-				<div class="col col-sm-12 col-xs-12 resumenAviso">
+				<div class="col-sm-12 col-12 resumenAviso">
 					<p>
 					La Universidad la Salle, A.C., ubicada en Benjamín Franklin 45 Col. Condesa, C.P. 06140, México Ciudad de México, le informa que sus datos personales y datos personales sensibles, se utilizarán para identificación, operación, administración y aquellos tratamientos definidos en la Política de Privacidad, que sean necesarios para la prestación de los servicios académicos y administrativos en el Sistema Educativo de las Universidades La Salle.</p>
 
@@ -65,10 +131,9 @@
 					<p>Si el titular proporciona sus datos personales significa que ha leído, entendido y aceptado los términos antes expuestos. <a href="http://www.lasalle.mx/privacidad" target="_blank"> Leer más</a></p>
 				</div>
 				
-				<div class="col-sm-12 col-xs-12 resumenTerminos">
+				<div class="col-sm-12 col-12 resumenTerminos">
 					<p>El uso por parte de cualquier persona (en adelante el USUARIO) del sitio web cuya dirección electrónica es: <a href="www.lasalle.mx"> www.lasalle.mx</a>, así como de los subsitios web que se deriven o estén sustentados en dicho sitio web, o de los cuales también sea titular la Universidad La Salle A. C. (en adelante el SITIO), constituye la aceptación y acuerdo pleno e incondicional por parte del USUARIO con la Universidad La Salle A. C. (en adelante UNIVERSIDAD LA SALLE), en relación a los presentes Términos y Condiciones de Uso del Sitio y subsitios. <a href="http://www.lasalle.mx/terminos-y-condiciones/" target="_blank">Leer más</a></p>
 				</div>
-			</div>
 		</div>
 	</footer>
 
@@ -83,34 +148,9 @@
 		ga('send', 'pageview');
 
 	</script>
-
-
-    <!-- jQuery -->
-    <script src="lib/jquery/jquery-3.2.1.min.js"></script>
-    <!--<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>-->
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
-    <!--<script src="lib/bootstrap/js/bootstrap.min.js"></script>-->
-
-    <!-- Plugin JavaScript -->
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
-
-    <!-- Theme JavaScript -->
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-
-    <script src="js/home.min.js"></script>
-
-	<!-- JS validator -->
-	<script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/jquery.validate.min.js"></script>
-	<script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/additional-methods.min.js"></script>
-	<script type="text/javascript" charset="utf-8"  src="js/validation.min.js"></script>
-
-		<!--Droplist -->
-	<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
-	
-
+	<!--js-->
+	<script type="text/javascript" charset="utf-8"  src="js/jquery-3.1.1.min.js"></script>
+	<script type="text/javascript" charset="utf-8"  src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" charset="utf-8"  src="js/home.js"></script>
 </body>
 </html>
