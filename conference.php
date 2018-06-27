1315b1f1e1b193215a58c37efd3c8ea0f873ae0d
Rukusu <nesesitounos@hotmail.com>
2018-06-27 18:57:15 -0500

    Directory

---
+++ conference.php
@@ -0,0 +1,266 @@
+<!DOCTYPE html>
+<html lang="es-419">
+
+<head>
+
+    <!--Metas -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Strength in Diversity">
+    <meta name="author" content="La Salle México">
+
+	<!--JS-->
+	<script type="text/javascript" src="js/afterglow.min.js"></script>
+
+    <!--Facebook -->
+    <meta property="og:url" content="http://www.lasalle.mx/ialu"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:image" content="http://www.lasalle.mx/ialu/images/home/IALULanding_Strength.png" />
+    <meta property="og:title" content="Encuetro IALU 2018 | La Salle" />
+    <meta property="og:description" content="Strength in Diversity">
+
+    <!-- Twitter-->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@LaSalle_MX">
+    <meta name="twitter:creator" content="@LaSalle_MX">
+    <meta name="twitter:title" content="Encuetro IALU 2018 | La Salle">
+    <meta name="twitter:description" content="Strength in Diversity">
+    <meta name="twitter:image" content="http://www.lasalle.mx/ialu/images/home/IALULanding_Strength.png">
+
+    <title>Encuetro IALU 2018 | La Salle</title>
+
+    <!-- Bootstrap Core CSS -->
+    <!--<link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- Web Fonts -->
+
+    <!-- Plugin CSS -->
+
+
+    <!-- Theme CSS -->
+    <link href="css/custom.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/2.9.0/css/flag-icon.min.css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <link rel="shortcut icon" href="images/favicon/favicon.ico" type="image/x-icon">
+</head>
+
+<body id="page-top">
+
+<header class="container">
+    <nav class="navbar navbar-expand-lg fixed-top navbar-light ">
+     <a class="navbar-brand" href="#"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse justify-content-center navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-item nav-link" href="#home">Home</a>
+        <a class="nav-item nav-link" href="#program">Program</a>
+		<a class="nav-item nav-link" href="#media">Media</a> 
+        <a class="nav-item nav-link" href="#director">Director</a>
+		<a class="nav-item nav-link" href="#download">Download</a> 		
+		<!--<a class="nav-item nav-link" href="#accomodation">Accommodations</a>-->
+        <!--<a class="nav-item nav-link" href="#applications">Submissions</a>-->
+        <!--<a class="nav-item nav-link" href="#registrationForm">Registration Form</a>-->
+      </div>
+    </div>
+  </nav>
+</header>
+
+<!-- Slider -->
+<section id="home" class="container-fluid">
+  <div class="row justify-content-center">
+    <div class="col-12">
+      <figure><img src="images/home/IALULanding_Strength.png" alt="Strength Diversity" class="img-fluid"></figure>
+    </div>
+  </div>
+  <div class="row justify-content-end">
+    <div class="col-sm-4"><figure><img src="images/home/IALULanding_EncuentroIALU.png" alt="Encuentro IALU XII" class="img-fluid"></figure></div>
+    
+  </div>
+</section>
+
+
+<section id="expos" class="container-fluid">
+<!--
+<div class="container">
+  <div class="row">
+    <div class="col">
+		<br>
+		<img src="images/gallery/RobertSchieler.jpg" alt="Robert Schieler">
+		<p>THE IMPACT OF LASALLIAN UNIVERSITIES: Three Touchstones to Identity</p>
+    </div>
+    <div class="col">
+		<br>
+		<img src="images/gallery/HansDeWit.jpg" alt="Hans de Wit">
+		<p>The why, what and how of Internationalization in higher education, implications of Catholic universities</p>
+    </div>
+    <div class="col">
+		<br>
+		<img src="images/gallery/GustavoRamirez.jpg" alt="Gustavo Ramírez Barba">
+		<p>#300LaSalle</p>
+    </div>
+	<div class="col">
+		<br>
+		<img src="images/gallery/ashoka.png" alt="ASHOKA">
+		<p>Transformation Agents, opportunity for the future</p>
+    </div>
+	<div class="col">
+		<br>
+		<img src="images/gallery/JosepPique.jpg" alt="Josep Piqué">
+		<p>From La Salle Innovation Park to BIT La Salle</p>
+    </div>
+  </div>
+</div>
+-->
+<div class="container">
+	<div class="row card-deck">
+			<div class="card" style="width: 18rem;">
+				<img class="center" src="images/gallery/RobertSchieler.jpg" alt="Robert Schieler" width="100px" height="100px">
+				<div class="card-body">
+					<h5 class="card-title">Robert Schieler</h5>
+					<p class="card-text">THE IMPACT OF LASALLIAN UNIVERSITIES: Three Touchstones to Identity</p>
+					<a href="#" class="btn btn-primary">Download</a>
+				</div>
+			</div>
+				
+			<div class="card" style="width: 18rem;">
+			  <img class="center" src="images/gallery/HansDeWit.jpg" alt="Hans de Wit" width="100px" height="100px">
+			  <div class="card-body">
+				<h5 class="card-title">Hans de Wit</h5>
+				<p class="card-text">The why, what and how of Internationalization in higher education, implications of Catholic universities</p>
+				<a href="#" class="btn btn-primary">Download</a>
+			  </div>
+			</div>
+				
+			<div class="card" style="width: 18rem;">
+			  <img class="center" src="images/gallery/GustavoRamirez.jpg" alt="Gustavo Ramírez Barba" width="100px" height="100px">
+			  <div class="card-body">
+				<h5 class="card-title">Gustavo Ramírez Barba</h5>
+				<p class="card-text">#300LaSalle</p>
+				<a href="#" class="btn btn-primary">Download</a>
+			  </div>
+			</div>
+	</div>
+	<br>
+	<div class="row card-deck">
+			<div class="card" style="width: 18rem;">
+			  <img class="center" src="images/gallery/ashoka.png" alt="ASHOKA" width="100px" height="100px">
+			  <div class="card-body">
+				<h5 class="card-title">Victor Moctezuma, Violeta Sandoval, María José Céspedes</h5>
+				<p class="card-text">Transformation Agents, opportunity for the future</p>
+				<a href="#" class="btn btn-primary">Download</a>
+			  </div>
+			</div>
+			<div class="card" style="width: 18rem;">
+			  <img class="center" src="images/gallery/JosepPique.jpg" alt="Josep Piqué" width="100px" height="100px">
+			  <div class="card-body">
+				<h5 class="card-title">Josep Piqué</h5>
+				<p class="card-text">From La Salle Innovation Park to BIT La Salle</p>
+				<a href="#" class="btn btn-primary">Download</a>
+			  </div>
+			</div>
+			<div class="card" style="width: 18rem;">
+			  <img class="center" src="images/gallery/DelvinDiaz.jpg" alt="Delvin Díaz" width="100px" height="100px">
+			  <div class="card-body">
+				<h5 class="card-title">Delvin Díaz Membreño</h5>
+				<p class="card-text">The Role of ULSA in Environmental Sustainability and the Promotion of Renewable Energies</p>
+				<a href="#" class="btn btn-primary">Download</a>
+			  </div>
+			</div>
+	</div>	
+	<br>
+	<div class="row card-deck">
+			<div class="card" style="width: 18rem;">
+			  <img class="center" src="images/gallery/MarcoSaez.png" alt="Marco Saez" width="100px" height="100px">
+			  <div class="card-body">
+				<h5 class="card-title">Marco Saez</h5>
+				<p class="card-text">The name of your conference: Paying Homage to DLSU-D's "Hatid-Aral" Program</p>
+				<a href="#" class="btn btn-primary">Download</a>
+			  </div>
+			</div>
+			<div class="card" style="width: 18rem;">
+			  <img class="center" src="images/gallery/EduardoGomez.jpg" alt="Eduardo Gómez " width="100px" height="100px">
+			  <div class="card-body">
+				<h5 class="card-title">Eduardo Gómez</h5>
+				<p class="card-text">Increasing visibility and enhancing impact research of Lasallian International Network</p>
+				<a href="#" class="btn btn-primary">Download</a>
+			  </div>
+			</div>
+			<div class="card" style="width: 18rem;">
+			  <img class="center" src="images/gallery/JackCurran.png" alt="Hno. Jack Curran" width="100px" height="100px">
+			  <div class="card-body">
+				<h5 class="card-title">Hno. Jack Curran</h5>
+				<p class="card-text">Strength in Diversity: Affirming Lasallian Catholic Hallmarks among the Faculty</p>
+				<a href="#" class="btn btn-primary">Download</a>
+			  </div>
+			</div>
+	</div>	
+	<br>
+	<div class="row card-deck">
+			<div class="card" style="width: 18rem;">
+			  <img class="center" src="images/gallery/MarthaBohorquez.png" alt="Martha Bohorquez" width="100px" height="100px">
+			  <div class="card-body">
+				<h5 class="card-title">Martha Bohorquez</h5>
+				<p class="card-text">Program of accompaniment to the orphanage jose mercado aguado in santa cruz de la sierra</p>
+				<a href="#" class="btn btn-primary">Download</a>
+			  </div>
+			</div>
+			<div class="card" style="width: 18rem;">
+			  <img class="center" src="images/gallery/FernandoOrlandi.jpg" alt="Fernando Cezar Orlandi" width="100px" height="100px">
+			  <div class="card-body">
+				<h5 class="card-title">Fernando Cezar Orlandi</h5>
+				<p class="card-text">Institutional support to chemical dependents for cognitive and social development</p>
+				<a href="#" class="btn btn-primary">Download</a>
+			  </div>
+			</div>
+			<div class="card" style="width: 18rem;">
+			  <img class="center" src="images/gallery/MarieLummerzheim.jpg" alt="Marie Lummerzheim" width="100px" height="100px">
+			  <div class="card-body">
+				<h5 class="card-title">Marie Lummerzheim</h5>
+				<p class="card-text">Academic Mobility Programmes</p>
+				<a href="#" class="btn btn-primary">Download</a>
+			  </div>
+			</div>
+	</div>	
+	<br>
+	<div class="row card-deck">
+			<div class="card" style="width: 18rem;">
+			  <img class="center" src="images/gallery/GiovaniAnzola.jpg" alt="Giovani Anzola Pardo" width="100px" height="100px">
+			  <div class="card-body">
+				<h5 class="card-title">Giovani Anzola Pardo</h5>
+				<p class="card-text">he golden circle of praxis of internationalization </p>
+				<a href="#" class="btn btn-primary">Download</a>
+			  </div>
+			</div>
+			<div class="card" style="width: 18rem;">
+			  <img class="center" src="images/gallery/DianaLoera.jpg" alt="Diana Loera" width="100px" height="100px">
+			  <div class="card-body">
+				<h5 class="card-title">Diana Loera </h5>
+				<p class="card-text">La SALLE SUMMER PROGRAM Bringing together Lasallian students from all over the world.</p>
+				<a href="#" class="btn btn-primary">Download</a>
+			  </div>
+			</div>
+			<div class="card" style="width: 18rem;">
+			  <img class="center" src="images/gallery/EleazarOrtiz.jpg" alt="Eleazar Ortiz" width="100px" height="100px">
+			  <div class="card-body">
+				<h5 class="card-title">Eleazar Ortiz</h5>
+				<p class="card-text">Technology for collaboration</p>
+				<a href="#" class="btn btn-primary">Download</a>
+			  </div>
+			</div>
+	</div>	
+		
+	</div>
+</div>
+</section>
+
+<?php include 'footer.php';?>
